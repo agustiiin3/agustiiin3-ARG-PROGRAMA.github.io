a9897487d18a06ea6cdc557263cc396e58f6ff4f
Agustín Fernández <146673694+agustiiin3@users.noreply.github.com>
2023-10-02 05:32:10 -0300

    Corrigiendo CSS y Javascript
    Cambiando la función de JS para mostrar contacto y cambiando colores. Letra de Google corregida

---
+++ js/cambiar_texto.js
@@ -0,0 +1,3 @@
+document.getElementById("demo").addEventListener("click",cambiar_texto() { 
+    console.log("Hola, el usuario hizo clic para ver Nº telefonico");
+    document.getElementById("demo").innerHTML = "El contacto de Whatsapp es +5492215928268";  });
